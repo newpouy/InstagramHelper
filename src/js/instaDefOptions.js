613ee64b19005395710c9917b146dbe6f0728c2f
ok <oleg.koleskin@gmail.com>
2018-04-14 18:44:13 +0300

    changed checkbox regexp

--- src/js/instaDefOptions.js
+++ src/js/instaDefOptions.js
@@ -16,7 +16,8 @@ var instaDefOptions = (function () {
     retryInterval: 180000,
     regFindUser: /link rel="canonical" href="https:\/\/www.instagram.com\/([^"/]*)\/?"/i,
     regExtractUserName: /instagram.com.*\/(?:\?taken-by=)?([^/?]+)\/?/, //fixes #10, respects the URL with parameters
-    regCheckBox: /^\s*<\s*input.+type\s*=\s*"checkbox".+value\s*=\s*(?:"|')\s*(true|false)/i,
+    //regCheckBox: /^\s*<\s*input.+type\s*=\s*"checkbox".+value\s*=\s*(?:"|')\s*(true|false)/i,
+    regCheckBox: /checkbox[\s\S]*value\s*=\s*(?:"|')(true|false)/i,
     regProfile: /^\s*<\s*a\s.*href\s*=\s*(?:"|')([^"']+)/i,
     regTestInfo: /^\s*id:/,
     cleanInfo: /<\/?.[^>]*>/g,
