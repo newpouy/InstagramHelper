e4307e0b1eed6f212c60c06a1619533a7c599ea7
OllegK <oleg.koleskin@gmail.com>
2019-02-07 19:32:36 +0200

    WIP

--- src/js/background.js
+++ src/js/background.js
@@ -3,6 +3,38 @@
 (function () {
   'use strict';
 
+  console.log('background script executed')
+  chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+    console.log(token);
+    if (chrome.runtime.lastError) {
+      alert(chrome.runtime.lastError.message);
+      return;
+    }
+    var x = new XMLHttpRequest();
+    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
+    x.onload = function () {
+      alert(x.response);
+    };
+    x.send();
+    console.log(chrome.runtime.id);
+    var CWS_LICENSE_API_URL = 'https://www.googleapis.com/chromewebstore/v1.1/userlicenses/';
+    var req = new XMLHttpRequest();
+    req.open('GET', CWS_LICENSE_API_URL + chrome.runtime.id);
+    req.setRequestHeader('Authorization', 'Bearer ' + token);
+    req.onreadystatechange = function () {
+      if (req.readyState == 4) {
+        var license = JSON.parse(req.responseText);
+        // verifyAndSaveLicense(license);
+        console.log(license);
+      }
+    };
+    req.onload = function () {
+      console.log(this.status);
+      console.log(this.response);
+    }
+    req.send();
+  });
+
   const promiseChrome = new PromiseChrome();
 
   chrome.runtime.onInstalled.addListener((details) => {
