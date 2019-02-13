b3db36f0647152ba8c3f7414be16f5b6fd116789
OlegKoleskin <oleg.koleskin@gmail.com>
2019-02-13 16:51:23 +0200

    WIP70

--- src/js/background.js
+++ src/js/background.js
@@ -4,37 +4,30 @@
   'use strict';
 
   console.log('background script executed')
-  chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+  chrome.identity.getAuthToken({
+    scopes: ['https://www.googleapis.com/auth/chromewebstore.readonly'],
+    'interactive': true
+  }, function (token) {
     console.log(token);
+    console.log(chrome.runtime.id);
     if (chrome.runtime.lastError) {
       alert(chrome.runtime.lastError.message);
       return;
     }
-    var x = new XMLHttpRequest();
-    x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
-    x.onload = function () {
-      alert(x.response);
-      setTimeout( () => {
-        console.log(chrome.runtime.id);
-        var CWS_LICENSE_API_URL = 'https://www.googleapis.com/chromewebstore/v1.1/userlicenses/';
-        var req = new XMLHttpRequest();
-        req.open('GET', CWS_LICENSE_API_URL + chrome.runtime.id);
-        req.setRequestHeader('Authorization', 'Bearer ' + token);
-        req.onreadystatechange = function () {
-          if (req.readyState == 4) {
-            var license = JSON.parse(req.responseText);
-            // verifyAndSaveLicense(license);
-            console.log(license);
-          }
-        };
-        req.onload = function () {
-          console.log(this.status);
-          console.log(this.response);
+    setTimeout(() => {
+      const cws_license_api_url = `https://www.googleapis.com/chromewebstore/v1.1/userlicenses/${chrome.runtime.id}`;
+
+      fetch(cws_license_api_url, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      }).then((response) => {
+        if (response.status !== 200) {
+          console.error(`response status: ${response.status}`);
+          console.error(`response: ${JSON.stringify(response.json)}`);
+        } else {
+          return response.json();
         }
-        req.send();
-      }, 5000);
-    };
-    x.send();
+      });
+    }, 5000)
 
   });
 
